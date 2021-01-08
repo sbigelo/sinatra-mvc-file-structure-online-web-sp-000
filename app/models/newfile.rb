6ac5361178b8e60105aae046c01aae7219383628
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-08 18:00:21 +0000

    Automatically backed up by Learn

--- app/models/newfile.rb
+++ app/models/newfile.rb
@@ -1,6 +1,14 @@
 class Dog
 
+attr_accessor :name, :breed, :age
+
 @@all = []
+
+  def initialization(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
   def self.all
     @@all
